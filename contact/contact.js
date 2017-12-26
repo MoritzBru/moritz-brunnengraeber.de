9a610b2d15cc1b8586529846d161814fc67b7803
Moritz <moritz_brunnengraeber@hotmail.com>
2017-12-26 03:23:30 +0100

    contact js merge functions

--- contact/contact.js
+++ contact/contact.js
@@ -1,10 +1,10 @@
-$(function () {
-  "use strict";
+(function($) {
+  "use strict"; // Start of use strict
 
   // Autoresize textares
-  autosize($('textarea'));
+  autosize($("textarea"));
 
-  var form = $('#contact_form')[0];
+  var form = $("#contact_form")[0];
 
   // Custom JavaScript for Validation
   form.addEventListener("submit", function(e) {
@@ -13,17 +13,6 @@ $(function () {
       e.stopPropagation();
     }
     form.classList.add("was-validated");
-  }, false); // on submit
-  form.addEventListener("reset", function(e) {
-    form.reset();
-    var alertBox = "<div class='alert " + "alert-danger" + " alert-dismissable fade show'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>" + "test" + "</div>";
-    form.querySelector('.contact_response').innerHTML = alertBox;
-    form.classList.remove("was-validated");
-  }, false); // on reset
-
-
-  // when the form is submitted
-  form.addEventListener("submit", function(e) {
 
     // if the validator does not prevent form submit
     if (!e.defaultPrevented) {
@@ -56,5 +45,48 @@ $(function () {
       }); // ajax
       return false;
     }
-  })
-});
+
+  }, false); // on submit
+  form.addEventListener("reset", function(e) {
+    form.reset();
+    form.classList.remove("was-validated");
+  }, false); // on reset
+
+
+  // when the form is submitted
+  // form.addEventListener("submit", function(e) {
+  // // $("contact_form").submit(function( event ) {
+  //
+  //   // if the validator does not prevent form submit
+  //   if (!e.defaultPrevented) {
+  //     var url = "contact/contact.php";
+  //
+  //     // POST values in the background the the script URL
+  //     $.ajax({
+  //       type: "POST",
+  //       url: url,
+  //       data: $(this).serialize(),
+  //       success: function (data) { // data = JSON object that contact.php returns
+  //
+  //         // we recieve the type of the message: success x danger and apply it to the
+  //         var messageAlert = "alert-" + data.type;
+  //         var messageText = data.message;
+  //
+  //         // let's compose Bootstrap alert box HTML
+  //         var alertBox = "<div class='alert " + messageAlert + " alert-dismissable fade show'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>" + messageText + "</div>";
+  //
+  //         // If we have messageAlert and messageText
+  //         if (messageAlert && messageText) {
+  //           // inject the alert to .messages div in our form
+  //           form.querySelector('.contact_response').innerHTML = alertBox;
+  //           // empty the form if message was sent
+  //           if (data.type == "success") {
+  //             form.reset();
+  //           }
+  //         }
+  //       }
+  //     }); // ajax
+  //     return false;
+  //   }
+  // })
+})(jQuery); // End of use strict
