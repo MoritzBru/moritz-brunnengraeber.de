dee8069c50eb96482b8a78488b9d1e61ede8d370
Moritz <moritz_brunnengraeber@hotmail.com>
2017-12-26 01:41:08 +0100

    clean up contact php

--- contact/contact.js
+++ contact/contact.js
@@ -7,56 +7,52 @@ $(function () {
   var form = $('#contact_form')[0];
 
   // Custom JavaScript for Validation
-  window.addEventListener("load", function() {
-    form.addEventListener("submit", function(event) {
-      if (form.checkValidity() == false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      form.classList.add("was-validated");
-    }, false); // on submit
-    form.addEventListener("reset", function(event) {
-      form.classList.remove("was-validated");
-    }, false); // on reset
-  }, false);
+  form.addEventListener("submit", function(e) {
+    if (form.checkValidity() == false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+    form.classList.add("was-validated");
+  }, false); // on submit
+  form.addEventListener("reset", function(e) {
+    form.classList.remove("was-validated");
+  }, false); // on reset
 
 
-    // when the form is submitted
+  // when the form is submitted
+  // $(form).on("submit", function (e) {
+  form.addEventListener("submit", function(e) {
 
-    $(form).on("submit", function (e) {
+    // if the validator does not prevent form submit
+    if (!e.isDefaultPrevented()) {
+      var url = "contact/contact.php";
 
-        // if the validator does not prevent form submit
-        if (!e.isDefaultPrevented()) {
-            var url = "contact/contact.php";
+      // POST values in the background the the script URL
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: $(this).serialize(),
+        success: function (data) { // data = JSON object that contact.php returns
 
-            // POST values in the background the the script URL
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    // data = JSON object that contact.php returns
+          // we recieve the type of the message: success x danger and apply it to the
+          var messageAlert = "alert-" + data.type;
+          var messageText = data.message;
 
-                    // we recieve the type of the message: success x danger and apply it to the
-                    var messageAlert = "alert-" + data.type;
-                    var messageText = data.message;
+          // let's compose Bootstrap alert box HTML
+          var alertBox = "<div class='alert " + messageAlert + " alert-dismissable fade show'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>" + messageText + "</div>";
 
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = "<div class='alert " + messageAlert + " alert-dismissable fade show'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>" + messageText + "</div>";
-
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $(form).find(".messages").html(alertBox);
-                        // empty the form if message was sent
-                        if (data.type == "success") {
-                          form.reset();
-                        }
-                    }
-                }
-            });
-            return false;
+          // If we have messageAlert and messageText
+          if (messageAlert && messageText) {
+            // inject the alert to .messages div in our form
+            $(form).find(".messages").html(alertBox);
+            // empty the form if message was sent
+            if (data.type == "success") {
+              form.reset();
+            }
+          }
         }
-    })
+      }); // ajax
+      return false;
+    }
+  })
 });
