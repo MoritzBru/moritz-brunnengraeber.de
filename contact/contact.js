7d496f1b40b8c8562e4d2f9d7d90fc50d4a97318
Moritz <moritz_brunnengraeber@hotmail.com>
2017-12-26 13:09:01 +0100

    indent contact js

--- contact/contact.js
+++ contact/contact.js
@@ -21,42 +21,42 @@ $(function () {
   }, false);
 
 
-    // when the form is submitted
+  // when the form is submitted
 
-    $(form).on("submit", function (e) {
+  $(form).on("submit", function (event) {
 
-        // if the validator does not prevent form submit
-        if (!e.isDefaultPrevented()) {
-            var url = "contact/contact.php";
+    // if the validator does not prevent form submit
+    if (!event.isDefaultPrevented()) {
+      var url = "contact/contact.php";
 
-            // POST values in the background the the script URL
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    // data = JSON object that contact.php returns
+      // POST values in the background the the script URL
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: $(this).serialize(),
+        success: function (data)
+        {
+          // data = JSON object that contact.php returns
 
-                    // we recieve the type of the message: success x danger and apply it to the
-                    var messageAlert = "alert-" + data.type;
-                    var messageText = data.message;
+          // we recieve the type of the message: success x danger and apply it to the
+          var messageAlert = "alert-" + data.type;
+          var messageText = data.message;
 
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = "<div class='alert " + messageAlert + " alert-dismissable fade show'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>" + messageText + "</div>";
+          // let's compose Bootstrap alert box HTML
+          var alertBox = "<div class='alert " + messageAlert + " alert-dismissable fade show'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>" + messageText + "</div>";
 
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $(form).find(".messages").html(alertBox);
-                        // empty the form if message was sent
-                        if (data.type == "success") {
-                          form.reset();
-                        }
-                    }
-                }
-            });
-            return false;
+          // If we have messageAlert and messageText
+          if (messageAlert && messageText) {
+            // inject the alert to .messages div in our form
+            $(form).find(".messages").html(alertBox);
+            // empty the form if message was sent
+            if (data.type == "success") {
+              form.reset();
+            }
+          }
         }
-    })
+      });
+      return false;
+    }
+  })
 });
