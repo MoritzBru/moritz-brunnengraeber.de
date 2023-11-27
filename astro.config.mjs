8fce331ee29d5d5be3f255ef39cf6202dbb32968
MoritzBru <moritz_brunnengraeber@hotmail.com>
2023-11-28 00:52:42 +0100

    feat: add integration, navbar, footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,23 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import Icons from 'unplugin-icons/vite';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://www.moritz-brunnengraeber.de',
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+        customCollections: {
+          mb: FileSystemIconLoader('./src/assets/icons'),
+        },
+      }),
+    ],
+  },
 });
--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import("prettier").Config} */
 export default {
-  // plugins: ['prettier-plugin-astro'],
-  // overrides: [
-  //   {
-  //     files: '*.astro',
-  //     options: {
-  //       parser: 'astro',
-  //     },
-  //   },
-  // ],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
   printWidth: 100,
   semi: true,
   singleQuote: true,
